68153fbe3f65d8cf15e1d3269ed5ba0fce71d0c6
ofekg <ofekgitmul@gmail.com>
2024-11-11 21:54:32 +0200

    h

---
+++ HumanPlayer.java
@@ -0,0 +1,10 @@
+public class HumanPlayer extends Player {
+    public HumanPlayer(boolean b) {
+        super(b);
+    }
+
+    @Override
+    boolean isHuman() {//is player 1
+        return true;
+    }
+}
