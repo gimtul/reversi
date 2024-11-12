03de057d988438b37ebb29e475a8f6cc8e0ec053
ofekg <ofekgitmul@gmail.com>
2024-11-12 19:43:00 +0200

    h

--- SimpleDisc.java
+++ SimpleDisc.java
@@ -1,19 +1,23 @@
 public class SimpleDisc implements Disc {
+
+    private Player player;
+
     public SimpleDisc(Player currentPlayer) {
+        this.player = currentPlayer;
     }
 
     @Override
     public Player getOwner() {
-        return null;
+        return this.player;
     }
 
     @Override
     public void setOwner(Player player) {
-
+        this.player = player;
     }
 
     @Override
     public String getType() {
-        return null;
+        return "Simple Disc";
     }
 }
