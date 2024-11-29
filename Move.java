07a6497dfdce8922303f23094d89b622e95af293
ofekg <ofekgitmul@gmail.com>
2024-11-29 16:29:04 +0200

    discs

--- Move.java
+++ Move.java
@@ -2,11 +2,11 @@ public class Move {
     private Position pos;
     private Disc disc;
     public Move(Position p,Disc d){
-       this.disc=d;
-       this.pos=p;
+        this.disc=d;
+        this.pos=p;
     }
     public Position position() {
-       return pos;
+        return pos;
     }
 
     public Disc disc() {
